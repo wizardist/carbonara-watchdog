bedd0b558a65d4a5c533aa8f9845284b7f5a8b11
Pavel Selitskas <p.selitskas@gmail.com>
2024-02-17 16:04:19 +0200

    skip past weeks and days while looking for carbonara

--- src/cli/bin/main.rs
+++ src/cli/bin/main.rs
@@ -1,5 +1,12 @@
+use std::ops::Add;
+use chrono::{Days, NaiveDate, TimeDelta, Utc, Weekday};
+use chrono_tz::Tz;
+
 const MENU_URL: &'static str =
     "https://www.raflaamo.fi/en/restaurant/helsinki/la-famiglia-helsinki/menu/lunch";
+
+/// Allows to calculate Friday for weeks starting on Monday.
+const FRIDAY_OFFSET: u64 = 4;
 
 #[tokio::main]
 async fn main() {
@@ -8,24 +15,50 @@ async fn main() {
     let json_data = body.await.unwrap();
     let v: serde_json::Value = serde_json::from_str(json_data.as_str()).unwrap();
 
+    let today = Utc::now().with_timezone(&Tz::Europe__Helsinki).date_naive();
+    let friday = |d: NaiveDate| d.week(Weekday::Mon).first_day().add(Days::new(FRIDAY_OFFSET));
+
     for week_menu in v["props"]["pageProps"]["initialApolloState"]["MenuListingItemRestaurant:369"]
         ["lunchMenuGroups"][0]["weeklyLunchMenu"]
         .as_array()
         .unwrap()
         .iter()
     {
-        let start_date = &week_menu["date"]["start"];
-        println!("{}", start_date);
+        let start_date = NaiveDate::parse_from_str(
+            &week_menu["date"]["start"].as_str().unwrap(),
+            "%Y-%m-%d",
+        ).unwrap();
+
+        // We don't need to check the week if today is past week's Friday or the whole week has passed.
+        let week_friday = friday(start_date);
+        if today.signed_duration_since(week_friday) > TimeDelta::zero() {
+            continue;
+        }
 
         const WEEKDAYS: [&'static str; 5] =
             ["monday", "tuesday", "wednesday", "thursday", "friday"];
 
-        for day in WEEKDAYS {
+        for (day_offset, day) in WEEKDAYS.iter().enumerate() {
+            let dish_date = start_date.add(Days::new(day_offset as u64));
+
+            // We don't need to check days that precede today.
+            if today.signed_duration_since(dish_date) > TimeDelta::zero() {
+                continue;
+            }
+
             let day_menu = &week_menu["dailyMenuAvailabilities"][day]["menu"];
-            println!(
-                "{}: {}",
-                day, day_menu["menuSections"][0]["portions"][0]["name"]["default"]
-            )
+            let dish = day_menu["menuSections"][0]["portions"][0]["name"]["default"]
+                .as_str().to_owned()
+                .unwrap();
+
+            if dish.to_lowercase().contains("carbonara") {
+                println!(
+                    "{}: {}",
+                    dish_date, dish
+                );
+
+                break;
+            }
         }
     }
 }
